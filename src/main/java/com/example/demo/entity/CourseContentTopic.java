d9642dba72e720a50364b4745adadd3849a1f9fc
bala-amypo <bala@amypo.in>
2025-12-18 08:11:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class CourseContentTopic {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String topicName;
+    private Double weightPercentage;
+
+    @ManyToOne
+    private Course course;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTopicName() { return topicName; }
+    public void setTopicName(String topicName) { this.topicName = topicName; }
+    public Double getWeightPercentage() { return weightPercentage; }
+    public void setWeightPercentage(Double weightPercentage) { this.weightPercentage = weightPercentage; }
+    public Course getCourse() { return course; }
+    public void setCourse(Course course) { this.course = course; }
+}
