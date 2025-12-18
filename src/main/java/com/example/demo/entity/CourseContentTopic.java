a55eeb79e6f61bca4726b1f45b125af0fb75bb71
bala-amypo <bala@amypo.in>
2025-12-18 08:16:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -14,12 +14,28 @@ public class CourseContentTopic {
     @ManyToOne
     private Course course;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getTopicName() { return topicName; }
-    public void setTopicName(String topicName) { this.topicName = topicName; }
-    public Double getWeightPercentage() { return weightPercentage; }
-    public void setWeightPercentage(Double weightPercentage) { this.weightPercentage = weightPercentage; }
-    public Course getCourse() { return course; }
-    public void setCourse(Course course) { this.course = course; }
+    public Long getId() { 
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTopicName() { 
+        return topicName;
+    }
+    public void setTopicName(String topicName) {
+        this.topicName = topicName; 
+    }
+    public Double getWeightPercentage() {
+        return weightPercentage; 
+    }
+    public void setWeightPercentage(Double weightPercentage) {
+        this.weightPercentage = weightPercentage;
+    }
+    public Course getCourse() {
+        return course;
+    }
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
