321776ec2a21e12b732576e284107c8630bc55c6
bala-amypo <bala@amypo.in>
2025-12-19 06:29:59 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UniversityServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.University;
+import com.example.demo.repository.UniversityRepository;
+import com.example.demo.service.UniversityService;
+
+@Service
+public class UniversityServiceImpl implements UniversityService {
+
+    private UniversityRepository repository;
+
+    @Override
+    public University createUniversity(University university) {
+        if (university.getName() == null || university.getName().isBlank()) {
+            throw new IllegalArgumentException("Name required");
+        }
+        if (repository.findByName(university.getName()).isPresent()) {
+            throw new IllegalArgumentException("University exists");
+        }
+        return repository.save(university);
+    }
+
+    @Override
+    public University updateUniversity(Long id, University university) {
+        University existing = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("University not found"));
+        existing.setName(university.getName());
+        return repository.save(existing);
+    }
+
+    @Override
+    public University getUniversityById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("University not found"));
+    }
+
+    @Override
+    public void deactivateUniversity(Long id) {
+        University u = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("University not found"));
+        u.setActive(false);
+        repository.save(u);
+    }
+}
