fb015e968947df775edadba28223c77c768f8142
bala-amypo <bala@amypo.in>
2025-12-22 08:30:48 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,24 +1,24 @@
-// package com.example.demo.exception;
+package com.example.demo.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-//     @ExceptionHandler(IllegalArgumentException.class)
-//     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
-//         return ResponseEntity
-//                 .status(HttpStatus.BAD_REQUEST)
-//                 .body(ex.getMessage());
-//     }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
 
-//     @ExceptionHandler(RuntimeException.class)
-//     public ResponseEntity<String> handleRuntime(RuntimeException ex) {
-//         return ResponseEntity
-//                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                 .body(ex.getMessage());
-//     }
-// }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
